9019cc8d138d4abb41e5e3e38715146144d31352
soliman kaddar <solim@Solimans-Laptop.local>
2025-02-08 00:26:09 -0700

    first commit

---
+++ SoftwareEngineeringApp/SoftwareEngineeringAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoftwareEngineeringAppApp.swift
+//  SoftwareEngineeringApp
+//
+//  Created by soliman kaddar on 2/3/25.
+//
+
+import SwiftUI
+
+@main
+struct SoftwareEngineeringAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SoftwareEngineeringAppTests/SoftwareEngineeringAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  SoftwareEngineeringAppTests.swift
+//  SoftwareEngineeringAppTests
+//
+//  Created by soliman kaddar on 2/3/25.
+//
+
+import Testing
+@testable import SoftwareEngineeringApp
+
+struct SoftwareEngineeringAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
